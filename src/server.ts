afc2110058b6044fa964acdffac8a90c55c333fd
João Vitor Marques <jvsilvam@outlook.com>
2022-10-05 12:35:41 -0300

    create ad table

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 const app = express();
+const prisma = new PrismaClient({
+  log: ['query'],
+});
+
+app.get('/api/games', async (request, response) => {
+
+  const games = await prisma.game.findMany();
+
+  return response.json({games});
+})
 
 app.listen(3333, () => {
   console.log('Server started at http://localhost:3333');
